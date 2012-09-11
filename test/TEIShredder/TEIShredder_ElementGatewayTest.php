9d634e06fedc149bbc405504f17d508af2e40726
Carsten Blüm <carsten@bluem.net>
2012-09-11 18:08:05 +0200

    Removed test for removed method findAll()

--- test/TEIShredder/TEIShredder_ElementGatewayTest.php
+++ test/TEIShredder/TEIShredder_ElementGatewayTest.php
@@ -29,15 +29,6 @@ class ElementGatewayTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @test
 	 */
-	function flushTheData() {
-		ElementGateway::flush($this->setup);
-		$objs = ElementGateway::findAll($this->setup);
-		$this->assertTrue(0 == count($objs));
-	}
-
-	/**
-	 * @test
-	 */
 	function saveANewElement() {
 		$element = new Element($this->setup);
 		$element->xmlid = "pb-15";
@@ -77,30 +68,22 @@ class ElementGatewayTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @test
+	 * @expectedException InvalidArgumentException
 	 */
-	function findAllElements() {
+	function flushTheData() {
 
+		// First, create object
 		$element = new Element($this->setup);
-		$element->xmlid = 'element-1';
-		$element->element = 'p';
-		$element->page = 100;
-		$element->chunk= 350;
+		$element->xmlid = "pb-15";
+		$element->element = 'div';
+		$element->page = 23;
+		$element->chunk = 234;
 		ElementGateway::save($this->setup, $element);
 
-		$element = new Element($this->setup);
-		$element->xmlid = 'element-2';
-		$element->element = 'lg';
-		$element->page = 200;
-		$element->chunk= 250;
-		ElementGateway::save($this->setup, $element);
+		ElementGateway::flush($this->setup);
 
-		$objs = ElementGateway::findAll($this->setup);
-		$this->assertInternalType('array', $objs);
-		$this->assertTrue(2 == count($objs));
-		$this->assertInstanceOf('\TEIShredder\Element', $objs[0]);
-		$this->assertInstanceOf('\TEIShredder\Element', $objs[1]);
-		$this->assertSame('lg', $objs[0]->element);
-		$this->assertSame('element-1', $objs[1]->xmlid);
+		// Now, we shouldn’t be able to find the element
+		ElementGateway::find($this->setup, 'pb-15');
 	}
 
 }
