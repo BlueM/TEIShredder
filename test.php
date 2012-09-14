f6b22d58b17f6b4ca978a9b49ec9024474f7f9f7
Carsten Blüm <carsten@bluem.net>
2012-09-14 13:36:44 +0200

    Removed namespace imports and added more comments

--- test.php
+++ test.php
@@ -1,36 +1,36 @@
 #!/usr/bin/php
 <?php
 
-use \TEIShredder\Setup;
-use \TEIShredder\XMLReader;
-use \TEIShredder\Indexer_Chunker;
-use \TEIShredder\Indexer_Extractor;
-
 require __DIR__.'/autoload.php';
 
 $path = __DIR__.'/test/Sample-1.xml';
 
 $xml = file_get_contents($path);
 
+// Init the database connection
 $db = new PDO('sqlite:'.__DIR__.'/test.sqlite');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$setup = new Setup($db);
+// Create the Setup/configuration/DI object
+$setup = new \TEIShredder\Setup($db);
 
 // Create an \TEIShredder\XMLReader instance to
 // be used by the extractor and the chunker.
-$xmlreader = new XMLReader;
+$xmlreader = new \TEIShredder\XMLReader;
 
-// Create chunks and structure
-$chunker = new Indexer_Chunker($setup, $xmlreader, $xml);
+// Create XML chunks and track the document structure
+$chunker = new \TEIShredder\Indexer_Chunker($setup, $xmlreader, $xml);
 $chunker->process();
 
-// Extract more information
-$extractor = new Indexer_Extractor($setup, $xmlreader, $xml);
+// Locate named entities and index occurrences of elements
+$extractor = new \TEIShredder\Indexer_Extractor($setup, $xmlreader, $xml);
 $extractor->process();
 
 
-// Show some information we have collected using the code above
+// Done.
+// Now, Show some information we have collected using the code above
+
+
 echo "\nSome information on ".basename($path).":\n";
 
 // 1) Volumes
