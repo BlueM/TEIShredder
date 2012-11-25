26233ef9857df069dbad866c687bcf4711a079e3
Carsten Blüm <carsten@bluem.net>
2012-11-25 12:42:23 +0100

    Added a test

--- test/TEIShredder/TEIShredder_NamedEntityGatewayTest.php
+++ test/TEIShredder/TEIShredder_NamedEntityGatewayTest.php
@@ -3,6 +3,7 @@
 namespace TEIShredder;
 
 use \TEIShredder;
+use PDO;
 use \InvalidArgumentException;
 
 require_once __DIR__.'/../bootstrap.php';
@@ -155,5 +156,50 @@ class NamedEntityGatewayTest extends \PHPUnit_Framework_TestCase
     public function tryingToFindANamedEntityByAnInvalidPropertyThrowsAnException()
     {
         $this->obj->find('invalid = 1');
+    }
+
+    /**
+     * @test
+     * @covers TEIShredder\NamedEntityGateway::findDistinctNotations
+     */
+    public function getAllDistinctNotations()
+    {
+        $pdoStatementMock = $this->getMockBuilder('PDOStatement')
+            ->setConstructorArgs(array())
+            ->getMock();
+        $pdoStatementMock->expects($this->once())
+            ->method('execute')
+            ->with(array('person', 12345));
+        $pdoStatementMock->expects($this->once())
+            ->method('fetchAll')
+            ->with(PDO::FETCH_ASSOC)
+            ->will(
+            $this->returnValue(
+                array(
+                    array('notation' => 'Name2', 'notationhash' => 'fb554b3bb6'),
+                    array('notation' => 'Name1', 'notationhash' => 'b38422f17e'),
+                    array('notation' => 'Name1', 'notationhash' => 'b38422f17e'),
+                    array('notation' => 'Name2', 'notationhash' => 'fb554b3bb6'),
+                    array('notation' => 'Name1', 'notationhash' => 'b38422f17e'),
+                )
+            )
+        );
+
+        $pdoMock = $this->getMockBuilder('PDO')
+            ->setConstructorArgs(array('sqlite::memory:'))
+            ->getMock();
+        $pdoMock->expects($this->once())
+            ->method('prepare')
+            ->will($this->returnValue($pdoStatementMock));
+
+        $reflm = new \ReflectionProperty($this->obj, 'db');
+        $reflm->setAccessible(true);
+        $reflm->setValue($this->obj, $pdoMock);
+
+        $actual = $this->obj->findDistinctNotations('person', 12345);
+        $this->assertInternalType('array', $actual);
+        $this->assertSame(2, count($actual));
+        $this->assertSame(array('Name1', 3, 'b38422f17e'), $actual[0]);
+        $this->assertSame(array('Name2', 2, 'fb554b3bb6'), $actual[1]);
     }
 }
