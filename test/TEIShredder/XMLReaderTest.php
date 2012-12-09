198b6b25d8ae977a67b5e78facef8e7ae696dfcf
Carsten Blüm <carsten@bluem.net>
2012-12-09 19:59:30 +0100

    Minor internal changes

---
+++ test/TEIShredder/XMLReaderTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace TEIShredder;
+
+use TEIShredder;
+use RuntimeException;
+
+require_once __DIR__.'/../bootstrap.php';
+
+/**
+ * Unit tests for TEIShredder\XMLReader.
+ *
+ * @package    TEIShredder
+ * @subpackage Tests
+ * @covers     TEIShredder\XMLReader
+ */
+class XMLReaderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var XMLReader
+     */
+    protected $reader;
+
+    /**
+     * Sets up the fixture
+     */
+    public function setUp()
+    {
+        $this->reader = new XMLReader;
+    }
+
+    /**
+     * Removes the fixture
+     */
+    public function tearDown()
+    {
+        unset($this->reader);
+    }
+
+    /**
+     * @test
+     */
+    public function getTheOpeningTagForAnEmptyNodeWithoutAttributes()
+    {
+        $this->reader->xml('<root><b /></root>');
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->assertSame('<b/>', $this->reader->nodeOpenString());
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function getTheOpeningTagForAnEmptyNodeWithAttributesAndNamespace()
+    {
+        $this->reader->xml(
+            '<root xmlns:abc="http://example.org/abc"><'.'b foo="bar" x="y" abc:bla="bla" /></root>'
+        );
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->assertSame(
+                '<'.'b foo="bar" x="y" abc:bla="bla"/>',
+                $this->reader->nodeOpenString()
+            );
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function getTheOpeningTagForANonEmptyNodeWithoutAttributes()
+    {
+        $this->reader->xml('<root><b>Hallo Welt</b></root>');
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->assertSame('<b>', $this->reader->nodeOpenString());
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function getTheOpeningTagForANonEmptyNodeWithAttributesAndNamespace()
+    {
+        $this->reader->xml(
+            '<root xmlns:abc="http://example.org/abc"><'.'b foo="bar" x="y" abc:bla="bla">Hallo Welt</b></root>'
+        );
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->assertSame(
+                '<'.'b foo="bar" x="y" abc:bla="bla">',
+                $this->reader->nodeOpenString()
+            );
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function checkEscapingOfSpecialCharsInAttributeValues()
+    {
+        $this->reader->xml('<root><ref target="http://www.example.com/?a=b&amp;c=d" /></root>');
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->assertSame(
+                '<ref target="http://www.example.com/?a=b&amp;c=d"/>',
+                $this->reader->nodeOpenString()
+            );
+        }
+    }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     */
+    public function tryingToGetTheOpeningTagForAClosingTagThrowsAnException()
+    {
+        $this->reader->xml('<root><b></b></root>');
+        while ($this->reader->read()) {
+            if ('root' == $this->reader->localName ||
+                XMLReader::END_ELEMENT != $this->reader->nodeType
+            ) {
+                continue;
+            }
+            $this->reader->nodeOpenString();
+        }
+    }
+}
